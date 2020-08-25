057e742b87404e0580e81d7de839ff519e8c2cc8
Gregory Hawman <g.hawman@gmail.com>
2020-08-25 14:13:03 -0700

    finished two sums problem

--- day_2/two_sums.py
+++ day_2/two_sums.py
@@ -11,6 +11,23 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
 """
+# pass 1
 class Solution:
     def twoSum(self, nums, target):
-        pass
+        # make a cache {}
+        cache = {}
+        # make an index pointer 
+        index = 0
+        # loop through
+        for i in nums:
+            # make a difference value
+            diff = target - i
+            # if the target - x = difference and diff is in cache (bingo that's it)
+            if diff in cache:
+                # return (index, cache[difference]
+                return(index, cache[diff])
+            # else add x to cache (cache[i] = index)
+            cache[i] = index
+            # increment index
+            index += 1
+
